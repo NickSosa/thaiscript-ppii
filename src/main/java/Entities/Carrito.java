4bce8ee382897b98bd5512e196aaa4d0e6f9b9aa
Alvaro Antonio Sanchez <121462858+Alvaro-Antonii@users.noreply.github.com>
2024-09-08 13:01:02 -0300

    Initial commit

---
+++ src/main/java/Entities/Carrito.java
@@ -0,0 +1,46 @@
+package Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Getter
+@Setter
+@Table(name = "carrito")
+public class Carrito {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idcarrito")
+    private int idCarrito;
+
+    @Column(name = "idcliente")
+    private int idCliente;
+
+    @Column(name = "idproducto")
+    private int idProducto;
+
+    @Column(name="fecha", length = 45)
+    private String fecha;
+
+    @Column(name = "cantidad", length = 45)
+    private String cantidad;
+
+    @Column(name = "precio", length = 45)
+    private String precio;
+
+    @Column(name = "subtotal", length = 45)
+    private String subtotal;
+    @OneToMany(mappedBy="carrito", cascade = CascadeType.PERSIST)
+    private List<Producto> detalle = new ArrayList<Producto>();
+}
---
+++ src/main/java/Entities/Producto.java
@@ -0,0 +1,52 @@
+package Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "producto")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idproducto")
+    private int idProducto;
+
+    @Column(name = "idcliente")
+    private int idCliente;
+
+    @Column(name = "idcarrito")
+    private int idCarrito;
+
+    @Column(name = "fecha")
+    private String fecha;
+
+    @Column(name = "cantidad")
+    private String cantidad;
+
+    @Column(name = "precio")
+    private String precio;
+    
+    @OneToMany(mappedBy="producto", cascade = CascadeType.PERSIST)
+    private List<DetallePedido> detalle = new ArrayList<DetallePedido>();
+    
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+    		name="producto_categoria",
+    		joinColumns= @JoinColumn(name= "producto_id"),
+    		inverseJoinColumns = @JoinColumn(name= "categoria_id"))
+    private List<Categoria> categorias = new ArrayList<Categoria>();
+
+    @Column(name = "subtotal")
+    private String subtotal;
+
+}
